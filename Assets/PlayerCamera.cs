4a34a223306d5573e6093a85ca2929e0183468f0
Dergo32 <tranderrick32@gmail.com>
2018-04-30 09:06:37 -0700

    Added script to camera

---
+++ Assets/PlayerCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour {
+
+	public Transform player;
+
+	public float smooth = 0.1f;
+
+	public Vector3 Offset;
+
+	void LateUpdate(){
+
+		Vector3 targetPos = player.position + Offset;
+		transform.position = player.position + Offset;
+	}
+
+}
