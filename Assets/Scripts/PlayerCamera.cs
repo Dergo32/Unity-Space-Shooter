422841303f83b80c173fee787cd47e0ca03e3058
Dergo32 <tranderrick32@gmail.com>
2018-05-02 08:27:43 -0700

    Finsihed camera follow script
    Camera:
    - Camera now looks at player correctly
    
    - Added camera smoothing
    
    Other:
    - Player engine flares now align correctly with engine

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour {
+
+	public Transform player;
+
+	public float smoothSpeed = 10f;
+
+	public Vector3 Offset;
+
+	void FixedUpdate(){
+
+		Vector3 targetPos = player.position + Offset;	//know where the camera needs to go, including with desired offset
+		Vector3 smoothedPos = Vector3.Lerp (transform.position, targetPos, smoothSpeed*Time.deltaTime);	//apply smoothing (multiply by delta time so that speed is the same regardless of frame rate)
+		transform.position = targetPos;		//make the camera go where it needs to go
+	}
+
+}
