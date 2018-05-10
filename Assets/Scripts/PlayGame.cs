18b6b13fdcbd7bd3ba771c4f55e0178d2d6c5fac
Dergo32 <tranderrick32@gmail.com>
2018-05-10 09:05:16 -0700

    Fixed broken scripts

--- Assets/Scripts/PlayGame.cs
+++ Assets/Scripts/PlayGame.cs
@@ -7,11 +7,7 @@ public class PlayGame : MonoBehaviour {
 
 	public void loadnextlevel()
 	{
-<<<<<<< HEAD
-		SceneManager.LoadScene ("Level01", LoadSceneMode.Single);
-=======
 		SceneManager.LoadScene(2);	//use index in build settings
->>>>>>> 4a2978bfed1ccfc65e19d824af7d4577587896c6
 
 	}
 }
