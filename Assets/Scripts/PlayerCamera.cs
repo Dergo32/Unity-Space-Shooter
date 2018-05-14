16b84921a24928935bd07128f1ffd75341c570af
Dergo32 <tranderrick32@gmail.com>
2018-05-14 09:04:47 -0700

    Rewrote Camera and Player Movement Scripts
    - Rewrote player script to work together with other scripts
    
    - Rewrote camera script to make camera scroll

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -6,13 +6,24 @@ public class PlayerCamera : MonoBehaviour {
 
 	public float smoothSpeed = 10f;
 
+	public float scrollSpeed = 1f;
+
 	public Vector3 Offset;
 
-	void FixedUpdate(){
+	// Use this for initialization
+	void Start () {
 
 		Vector3 targetPos = player.position + Offset;	//know where the camera needs to go, including with desired offset
-		Vector3 smoothedPos = Vector3.Lerp (transform.position, targetPos, smoothSpeed*Time.deltaTime);	//apply smoothing (multiply by delta time so that speed is the same regardless of frame rate)
+		Vector3 smoothedPos = Vector3.Lerp (transform.position, targetPos, smoothSpeed * Time.deltaTime);	//apply smoothing (multiply by delta time so that speed is the same regardless of frame rate)
 		transform.position = targetPos;		//make the camera go where it needs to go
+	
+	}
+
+	//Updates once per frame
+	void FixedUpdate(){
+
+		transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z + scrollSpeed);	//make camera go forward at certain speed
+
 	}
 
 }
