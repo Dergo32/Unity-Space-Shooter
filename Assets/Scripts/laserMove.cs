a691591f91f85a67482ae7b560d2ef5129806db4
Dergo32 <tranderrick32@gmail.com>
2018-05-31 08:49:12 -0700

    Created laser prefab to be used later

---
+++ Assets/Scripts/laserMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class laserMove : MonoBehaviour {
+
+	public Rigidbody laserBody;	//reference to laser gameobject
+	public float speed;			//how fast laser moves
+
+	void Start(){
+	
+		laserBody.velocity = transform.forward * speed;	//move laser forward
+	
+	}
+
+}
