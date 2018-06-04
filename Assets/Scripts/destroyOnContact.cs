223b959663a2d27a4b253a3173b2ead76371960b
Dergo32 <tranderrick32@gmail.com>
2018-06-04 08:54:02 -0700

    Added asteriod for further use

---
+++ Assets/Scripts/destroyOnContact.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class destroyOnContact : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Enemy") {
+		
+			return;
+		
+		}
+
+		Destroy (other.gameObject);		//order doesn't matter
+		Destroy (gameObject);
+	}
+
+}
---
+++ Assets/Scripts/randomRotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class randomRotate : MonoBehaviour {
+
+	public float tumble;
+	public Rigidbody asteriod;
+
+	// Use this for initialization
+	void Start () {
+		asteriod.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
